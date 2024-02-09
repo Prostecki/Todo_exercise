12ce23b7366051ccf04af07c9cb1ea3b873ee050
Mark Taratynov <mark.taratynov@gmail.com>
2024-02-09 12:28:34 +0100

    checkbox and task

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const addTaskButton = document.querySelector('.add-task');
 const taskList = document.querySelector('.doing-window');
+
 addTaskButton.addEventListener('click', () => {
-    const newTask = document.createElement('h1');
-    newTask.textContent = 'New task...'
-    taskList.appendChild(newTask);
+    const taskWindow = document.createElement('div');
+    taskWindow.classList.add('flex-box-container');
+
+
+    const newTask = document.createElement('div');
+    const checkBox = document.createElement('div');
+
+    newTask.classList.add('one-task');
+    checkBox.classList.add('checkbox');
+
+    newTask.textContent = 'New task...';
+
+    taskList.appendChild(taskWindow);
+    taskWindow.appendChild(newTask);
+    taskWindow.appendChild(checkBox);
+
     console.log('clicked and added!');
-})
+
+});
+
+
