aae1c62af7c7c462db3cb7f395aa8915e1856ccf
Mark Taratynov <mark.taratynov@gmail.com>
2024-02-19 18:10:06 +0100

    window task

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const addTaskButton = document.querySelector('.add-task');
 const taskWindow = document.querySelector('.wrapper');
-const submitTask = document.querySelector('submit-task');
+const submitTask = document.querySelector('.submit-task');
+const taskInput = document.querySelector('.task-input');
+const createTaskWindow = document.querySelector('.window-task')
 
 addTaskButton.addEventListener('click', () => {
     taskWindow.style.display = 'flex';
-    taskWindow.style.transition = '.4s'
+    taskWindow.style.transition = '.4s';
+    submitTask.addEventListener('click', () => {
+        taskWindow.style.display = 'none';
+    });
 });
 
+submitTask.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        const newTask = document.createElement('h1');
+        newTask.textContent = taskText;
+        taskWindow.appendChild(newTask);
+        taskInput.value = '';
+        createTaskWindow.style.display = 'none';
+        createTaskWindow.style.zIndex = -10;
+    }
+})
 
 // document.body.style.background = 'red';
 // setTimeout(() => {
