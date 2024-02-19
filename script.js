be1ac380a2ee8204e7b81a62c0df251b11222d7c
Mark Taratynov <mark.taratynov@gmail.com>
2024-02-19 17:48:37 +0100

    just a bit fixes

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 const addTaskButton = document.querySelector('.add-task');
-const taskList = document.querySelector('.doing-window');
+const taskWindow = document.querySelector('.wrapper');
+const submitTask = document.querySelector('submit-task');
 
 addTaskButton.addEventListener('click', () => {
-    const taskWindow = document.createElement('div');
-    taskWindow.classList.add('flex-box-container');
-
-
-    const newTask = document.createElement('div');
-    const checkBox = document.createElement('div');
-
-    newTask.classList.add('one-task');
-    checkBox.classList.add('checkbox');
-
-    newTask.textContent = 'New task...';
-
-    taskList.appendChild(taskWindow);
-    taskWindow.appendChild(newTask);
-    taskWindow.appendChild(checkBox);
-
-    console.log('clicked and added!');
-
+    taskWindow.style.display = 'flex';
+    taskWindow.style.transition = '.4s'
 });
 
 
+// document.body.style.background = 'red';
+// setTimeout(() => {
+//     document.body.style.background = '';
+//     document.body.style.transition = '0.8s';    
+// }, 2000);
